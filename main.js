f65bfd7df1c50004d3d37065e4673b3167e0889c
narimara <marinarasauce940@gmail.com>
2026-08-12 14:49:08 +1000

    added intro dialog
    Added a dialog element with a button which closes it

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+// browser loads index.html -> browser loads js -> open the dialog ->
+// user closes dialog -> audio system loads -> user clicks sound button
+// find our dialog (like declaring gameObject) and also the close button
+const introDialog = document.getElementById("intro-dialog");
+const introDialogCloseButton = document.getElementById("intro-dialog-close");
+
+//show the found element in browser console
+    // console.log(introDialog);
+
 //find test button (like declaring gameObject)
 const testButton = document.getElementById('test-button');
 // init our synth
 const synth = new Tone.Synth().toDestination();
+
+// Dialog
+introDialog.showModal();
+
+//close dialog when user clicks button for it
+introDialogCloseButton.addEventListener("click", function closeIntroDialog(){
+    introDialog.close();});
+
+
 
 //do something when the button is clicked
 testButton.addEventListener("click", playNote);
